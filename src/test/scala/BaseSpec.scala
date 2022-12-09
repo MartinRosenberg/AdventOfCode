934ec1af2d63ad8dc43b8e52fd3d640c1e6d82ce
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-12-09 16:12:05 -0600

    Add BaseSpec in case of testing lol

---
+++ src/test/scala/BaseSpec.scala
@@ -0,0 +1,10 @@
+package com.martinbrosenberg.adventofcode
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{OneInstancePerTest, TryValues}
+
+class BaseSpec extends AnyFreeSpec
+	with Matchers
+	with OneInstancePerTest
+	with TryValues
