6c93a01b0433e2b850d4296d0dcef7215dc4079b
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-12-09 18:14:29 -0600

    Second run at 2022 day 1

--- src/main/scala/year2022/day1/Part1.scala
+++ src/main/scala/year2022/day1/Part1.scala
@@ -1,18 +1,18 @@
 package com.martinbrosenberg.adventofcode
 package year2022.day1
 
-import scala.collection.immutable.Queue
-import scala.collection.mutable.ArrayBuffer
-import scala.io.{BufferedSource, Source}
+import scala.io.Source
+import scala.util.Using
 
 object Part1 extends App {
-  val resource = Source.fromResource("year2022/day1/input.txt")
-  val lines = resource.getLines().map(_.trim)
-  var elfTotals = Queue.empty[Int]
-  while (lines.nonEmpty) {
-    elfTotals :+= lines.takeWhile(!_.isBlank).map(_.toInt).sum
-  }
-  val maxCalories = elfTotals.max
-  resource.close()
-  println(maxCalories)
+  Using(Source.fromResource("year2022/day1/input.txt")) { resource =>
+    val lines = resource.getLines().map(_.trim)
+
+    var elfTotals = Vector.empty[Int]
+    while (lines.nonEmpty) {
+      elfTotals :+= lines.takeWhile(!_.isBlank).map(_.toInt).sum
+    }
+
+    elfTotals.max
+  }.foreach(println)
 }
--- src/main/scala/year2022/day1/Part2.scala
+++ src/main/scala/year2022/day1/Part2.scala
@@ -1,18 +1,18 @@
 package com.martinbrosenberg.adventofcode
 package year2022.day1
 
-import scala.collection.immutable.Queue
-import scala.collection.mutable.ArrayBuffer
-import scala.io.{BufferedSource, Source}
+import scala.io.Source
+import scala.util.Using
 
 object Part2 extends App {
-  val resource = Source.fromResource("year2022/day1/input.txt")
-  val lines = resource.getLines().map(_.trim)
-  var elfTotals = Queue.empty[Int]
-  while (lines.nonEmpty) {
-    elfTotals :+= lines.takeWhile(!_.isBlank).map(_.toInt).sum
-  }
-  val max3Calories = elfTotals.sortWith(_ > _).take(3).sum
-  resource.close()
-  println(max3Calories)
+  Using(Source.fromResource("year2022/day1/input.txt")) { resource =>
+    val lines = resource.getLines().map(_.trim)
+
+    var elfTotals = Vector.empty[Int]
+    while (lines.nonEmpty) {
+      elfTotals :+= lines.takeWhile(!_.isBlank).map(_.toInt).sum
+    }
+
+    elfTotals.sortWith(_ > _).take(3).sum
+  }.foreach(println)
 }
