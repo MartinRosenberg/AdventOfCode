c5b66c0ceb304a6f0730c5577ed666ad34646e50
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-12-09 23:24:05 -0600

    Complete day 2

---
+++ src/main/scala/year2022/day2/RockPaperScissors_Part2.scala
@@ -0,0 +1,29 @@
+package com.martinbrosenberg.adventofcode
+package year2022.day2
+
+import scala.io.Source
+import scala.util.Using
+
+object RockPaperScissors_Part2 extends App {
+  def getPoints(hint: String): Int = hint match {
+    case "B X" => 1
+    case "C X" => 2
+    case "A X" => 3
+
+    case "A Y" => 4
+    case "B Y" => 5
+    case "C Y" => 6
+
+    case "C Z" => 7
+    case "A Z" => 8
+    case "B Z" => 9
+  }
+
+  Using(Source.fromResource("year2022/day2/input.txt"))(
+    _.getLines()
+      .toVector.view
+      .map(_.trim)
+      .map(getPoints)
+      .sum
+  ).foreach(println)
+}
