5a1cbbc8ccfcb9045a7b0bd166a47652fb655d9f
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-12-09 22:58:51 -0600

    Add heap solution for day 1

--- src/main/scala/year2022/day1/CalorieCounting.scala
+++ src/main/scala/year2022/day1/CalorieCounting.scala
@@ -4,7 +4,7 @@ package year2022.day1
 import scala.io.Source
 import scala.util.Using
 
-object Day1 extends App {
+object CalorieCounting extends App {
   Using(Source.fromResource("year2022/day1/input.txt")) { resource =>
     val lines = resource.getLines().map(_.trim)
 
---
+++ src/main/scala/year2022/day1/CalorieCounting_Heap.scala
@@ -0,0 +1,22 @@
+package com.martinbrosenberg.adventofcode
+package year2022.day1
+
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Using
+
+object CalorieCounting_Heap extends App {
+  Using(Source.fromResource("year2022/day1/input.txt")) { resource =>
+    val lines = resource.getLines().map(_.trim)
+
+    val elfTotals = mutable.PriorityQueue.empty[Int]
+    while (lines.nonEmpty) {
+      elfTotals += lines.takeWhile(!_.isBlank).map(_.toInt).sum
+    }
+
+    (1 to 3).map(_ => elfTotals.dequeue())
+  }.foreach { top3 =>
+    println(s"Part 1: ${top3.head}")
+    println(s"Part 2: ${top3.sum}")
+  }
+}
