fac323b4d44e766f95ac4c08d0949a5f6b170a22
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-12-09 22:57:10 -0600

    Complete part 1 of day 2

---
+++ src/main/scala/year2022/day2/RockPaperScissors_Part1.scala
@@ -0,0 +1,30 @@
+package com.martinbrosenberg.adventofcode
+package year2022.day2
+
+import scala.io.Source
+import scala.util.Using
+
+object RockPaperScissors_Part1 extends App {
+  def getMovePoints(letter: String): Int = letter match {
+    case "A" | "X" => 1
+    case "B" | "Y" => 2
+    case "C" | "Z" => 3
+  }
+
+  def getOutcomePoints(opp: Int, self: Int): Int = self - opp match {
+    case 1 | -2 => 6
+    case 0 => 3
+    case 2 | -1 => 0
+  }
+
+  Using(Source.fromResource("year2022/day2/input.txt"))(
+    _.getLines()
+      .toVector.view
+      .map(_.trim)
+      .map(_.split("\\s"))
+      .map(_.map(getMovePoints))
+      .foldLeft(0) { case (total, Array(opp, self)) =>
+        total + self + getOutcomePoints(opp, self)
+      }
+  ).foreach(println)
+}
