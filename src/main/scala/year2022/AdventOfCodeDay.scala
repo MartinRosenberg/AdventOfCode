cd63d7f3bc6729a9cf9c8a8b8ab02729c8a4ec4d
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2024-12-08 23:24:05 -0600

    Move inputs

--- src/main/scala/year2022/AdventOfCodeDay.scala
+++ src/main/scala/year2022/AdventOfCodeDay.scala
@@ -3,7 +3,7 @@ package year2022
 
 trait AdventOfCodeDay(day: Int) extends App {
   val url: String                = s"https://adventofcode.com/2022/day/$day"
-  protected val inputUri: String = f"year2022/day$day%02d/input"
+  protected val inputUri: String = f"year2022/day$day%02d"
 
 //  def part1(): Unit
 //  def part2(): Unit
